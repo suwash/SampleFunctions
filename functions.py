77c253b9a807cd31d5990864617bc7a8b14a9830
Suwash <suwasha@student.unimelb.edu.au>
2018-03-16 12:20:42 +1100

    Revert "Removed original function file"
    This reverts commit 8d6d59a738a75103a83160bd0587368784627d98.

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+# Addition
+def addition(a, b):
+ 	summation = a + b
+
+ 	return summation
+
+# Subtraction
+def subtraction(a, b):
+
+	difference = a - b
+
+	return difference
+
+# Multiplication
+def division(a, b):
+
+	quotient = a / b
+
+	return quotient
+
+# Division
+
+def multiplication(a, b):
+
+	product = a * b
+
+	return product
