dffae58bc9f00212967d00155595bb3291fe83a6
Suwash <suwasha@student.unimelb.edu.au>
2018-03-16 12:04:13 +1100

    Two functions completed
    Completed addition and subtraction functions.

--- functions.py
+++ functions.py
@@ -1,9 +1,16 @@
 
 # Addition
+def addition(a, b):
+ 	summation = a + b
 
+ 	return summation
 
 # Subtraction
+def subtraction(a, b):
 
+	difference = a - b
+
+	return difference
 
 # Multiplication
 
